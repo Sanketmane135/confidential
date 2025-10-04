7ec7a56f3ab32c3a3e4ab6cdfa6961901d71b57a
Sanketmane135 <sanketmane0407@gmail.com>
2025-10-04 11:16:58 +0530

    3rd chnage

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -2,15 +2,16 @@
 
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LogoutButton() {
   return (
     <button onClick={() => signOut()}  className="flex items-center  text-lg bg-gray-200 hover:bg-gray-300 p-2 rounded cursor-pointer " >
           <Image src="/google.png" alt="icon" width={20} height={20} />
           
-          <a href="/#" className="text-gray-800 hover:text-white transition pl-2 pr-2  hover:bg-blue-600 rounded"> 
+          <Link href="/#" className="text-gray-800 hover:text-white transition pl-2 pr-2  hover:bg-blue-600 rounded"> 
             Sign In
-          </a>
+          </Link>
     </button>
   );
 }
